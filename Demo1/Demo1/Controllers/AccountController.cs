e45c2ab8c236510164489e6dd1daf61aad4ee7f9
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-11-12 20:43:39 +0700

    tuanokey

---
+++ Demo1/Demo1/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        public AccountController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IActionResult Index()
+        {
+            /* IEnumerable<Category> categories = _appDbContext.Categories.Include(p => p.Blogs);
+             return View(categories);*/
+            return View();
+        }
+        public IActionResult Register()
+        {
+           
+            return View();
+        }
+
+        
+    }
+}
